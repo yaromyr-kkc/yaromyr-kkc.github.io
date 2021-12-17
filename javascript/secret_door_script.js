c6f890e05a576cd38eea4857c19a5f36d4e5d9e2
YarKKCdev <95471622+YarKKCdev@users.noreply.github.com>
2021-12-17 22:45:56 +0100

    fixed code

--- javascript/secret_door_script.js
+++ javascript/secret_door_script.js
@@ -1,7 +1,7 @@
-var code = 'alert("Hello world!")'
+var code = "alert('Hello world!')"
 function checkCode(value){
     if (value == code){
-        location.href = ""
+        alert("eeee")
     }
     else {
         alert("Dat is de code niet.")
