74a59a8f7af7cfdda34f0bb08a5a0048d1ba7409
YarKKCdev <95471622+YarKKCdev@users.noreply.github.com>
2021-12-18 13:41:30 +0100

    localtion hreff

--- javascript/secret_door_script.js
+++ javascript/secret_door_script.js
@@ -1,7 +1,7 @@
 var code = "alert('Hello world!')"
 function checkCode(value){
     if (value == code){
-        localtion.href = "/secret/rooms/secret_room.html"
+        location.href = "/secret/rooms/secret_room.html"
     }
     else {
         alert("Dat is de code niet.")
