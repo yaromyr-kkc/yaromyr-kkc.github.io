239fa3a3cbfc7c28710d628796b8538876c27af2
YarKKCdev <95471622+YarKKCdev@users.noreply.github.com>
2021-12-18 14:20:17 +0100

    dddeeeddd

--- javascript/secret_door_script.js
+++ javascript/secret_door_script.js
@@ -1,7 +1,7 @@
 var code = "alert('Hello world!')"
 function checkCode(value){
     if (value == code){
-        location.href = "/secret/rooms/secret_room.html"
+        location.href = "/pages/secret/rooms/secret_room.html"
     }
     else {
         alert("Dat is de code niet.")
