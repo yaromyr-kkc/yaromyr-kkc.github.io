56b98b06f723aa7d604beeebd78b6fe4a83ebc6c
YarKKCdev <95471622+YarKKCdev@users.noreply.github.com>
2022-01-08 13:14:41 +0100

    class_manager

---
+++ javascript/class_manager.js
@@ -0,0 +1,9 @@
+function set_dark_mode() {
+    document.body.className = "dark_mode"
+}
+function set_light_mode() {
+    document.body.className = "light_mode"
+}
+function set_rainbow_mode() {
+    document.body.className = "rainbow_mode"
+}
